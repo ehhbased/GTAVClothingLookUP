1e20eb6fbcbbfabcfb64777af862e15ecfa81924
OnlyMisT <yobo8127@outlook.com>
2021-03-28 16:45:04 -0500

    Added code to compare two file of the opposite gender. useful for looking for names when doing outfit transfers

--- V-Clothing.py
+++ V-Clothing.py
@@ -1,32 +1,70 @@
 import json
-print('''
-* human readable names of GTA V clothing items.
+file2Open = input("Enter the filename to open: ")
 
-* Many items have "NO_LABEL" and "NULL" as their names, which probably means they aren't available for purchase in GTA Online clothes shops
-OR they're old enough to have their names hardcoded in game scripts.
+def oppositeGenderFile(a):
+    a = file2Open
+    if file2Open != 'masks':
+        if a.startswith('m'):
+            return 'fe' + file2Open
+        elif a.startswith('f'):
+            b = a.split('_')
+            b[0] = 'male'
+            return b[0] + '_' + b[1]
+        elif a[6] == 'f':
+            c = a.split('_')
+            c[1] = 'male'
+            return c[0] + '_' + c[1] + '_' + c[2]
+        elif a[6] == 'm':
+            d = a.split('_')
+            d[1] = 'female'
+            return d[0] + '_' + d[1] + '_' + d[2]
+    else:
+        return a
 
-* Files without a prefix (e.g. masks.json) are the clothing items for both mp_m_freemode_01 and mp_f_freemode_01 models.
+def openFile(fileName):
+    theFile = open('Jsonfiles\\' + fileName + '.json','r')
+    return theFile
 
-* Files prefixed with male_ are the clothing items for mp_m_freemode_01 model. (male character)
+def printID(a, fileName):
+    print("\nnumber of " + fileName + " textures: ", len(a))
+    for i in range(len(a)):
+        b = a[str(i)]
+        print(i, "=  Text Label: ", b["GXT"], "  In-store Name: " + b["Localized"])
 
-* Files prefixed with female_ are the clothing items for mp_f_freemode_01 model. (female character)
-
-* Files prefixed with props_male_ are the props for mp_m_freemode_01 model. (male props ie. glasses, watch, ears, bracelets)
-
-* Files prefixed with props_female_ are the props for mp_f_freemode_01 model. (female props ie. glasses, watch, ears, bracelets)
-
-''')
-file2Open = input("Enter the file to open: ")
 #opens the file
-FILE = open('Jsonfiles\\' + file2Open + '.json','r')
-jsondata = FILE.read()
+jsondata2 = openFile(oppositeGenderFile(file2Open)).read()
+jsondata = openFile(file2Open).read()
 
-#parse 
+#parse
 obj = json.loads(jsondata)
-IDnum = int(input("Enter the ID to look up: "))
-mylist = obj[str(IDnum)]
-print("number of textures: ", len(mylist))
+obj2 = json.loads(jsondata2)
 
-for i in range(len(mylist)):
-    list2 = mylist[str(i)]
-    print(i, "=  Code Name: ", list2["GXT"], "  In-store Name: " + list2["Localized"])
+#prints the length of ID's found in the JSON file
+if file2Open != 'masks':
+    print('\n'+file2Open + ' IDs : 0 -', len(obj) - 1)
+    print(oppositeGenderFile(file2Open) + ' IDs : 0 -', len(obj2) - 1)
+else:
+    print('\n'+file2Open[0:4] + ' IDs : 0 -', len(obj) - 1)
+
+errorChecking = False
+
+#checks the IDnum entered
+while not errorChecking:
+    IDnum = int(input("\nEnter the ID to look up: "))
+    if IDnum > len(obj) or len(obj2):
+        if IDnum > len(obj) and IDnum < len(obj2):
+            print('the entered value is larger than what\'s found in ' + file2Open + '\n')
+            mylist2 = obj2[str(IDnum)]
+            printID(mylist2, oppositeGenderFile(file2Open))
+            continue
+        elif IDnum > len(obj) and len(obj2):
+            if file2Open == 'masks':
+                print('the entered value is higher than whats found in ' + file2Open)
+            print('the entered value is higher than whats found in both ' + file2Open + ' and ' + oppositeGenderFile(file2Open) + '\n')
+            continue
+    mylist = obj[str(IDnum)]
+    mylist2 = obj2[str(IDnum)]
+
+    printID(mylist, file2Open)
+    printID(mylist2, oppositeGenderFile(file2Open))
+    errorChecking = True
